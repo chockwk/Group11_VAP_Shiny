fc08d5404f30a3519939bb0f97360895ace80223
Wan Kee <chockwk@gmail.com>
2024-03-04 21:39:20 +0800

    Sidebar Customization

--- Group11_VAP_ShinyApp/ui.R
+++ Group11_VAP_ShinyApp/ui.R
@@ -1,9 +1,12 @@
-pacman::p_load(shiny, tidyverse)
+pacman::p_load(tidyverse, ggplot2, plotly, lubridate, ggrepel, reshape2, scales, ggthemes)
 
+weather <- readRDS("data/rds/weather.rds")
 # Define UI for application that draws a histogram
 fluidPage(
-
     # Application title
     titlePanel("The Heat is On!"),
-
+    sidebarLayout(
+      sidebarPanel("Exploratory Data Analysis"),
+      mainPanel("Choropleth maps")
+    )
 )
