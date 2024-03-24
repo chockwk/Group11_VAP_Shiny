81bfa78ad385488e95ed6fb10086a35f8347b3b9
noelnomel <noel100293@gmail.com>
2024-03-24 18:52:13 +0800

    CDA update

--- Group11_VAP_ShinyApp/server.R
+++ Group11_VAP_ShinyApp/server.R
@@ -1,5 +1,5 @@
 pacman::p_load(shiny, tidyverse, ggrepel, DT, plotly, forecast, stats, zoo, shinyjs, ggstatsplot, gganimate, ggthemes,
-               sf, tmap, terra, viridis, sp, raster, ggrepel)
+               sf, tmap, terra, viridis, sp, raster, gstat, automap, ggrepel)
 
 # import data
 temp_data <-read_rds("data/rds/temperature.rds")
